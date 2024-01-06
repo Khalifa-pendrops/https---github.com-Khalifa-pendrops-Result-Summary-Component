f809ccdb279268d343d9d9d14d1cd18d5258cbe6
Chikezie Ilodigwe <dikee5200@gmail.com>
2024-01-06 11:23:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const continueBtn = document.querySelector(".btn");
+const percentRate = document.querySelector(".percent h2 p");
+const resultState = document.querySelector(".summary-state");
+const number = document.getElementById("#number");
+const percent = document.querySelector(".right p");
+
+continueBtn = addEventListener("click", function () {});
